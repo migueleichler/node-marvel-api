285d33d70bee43ba580a0444cd37e88245b87f9a
migueleichler <migueleichler23@gmail.com>
2021-03-07 13:05:05 -0300

    [ADD] Added middleware lib Morgan for logging.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var morgan = require('morgan')
 
+var app = express();
+
+
+app.use(morgan('tiny'));
 
 app.get('/hero', (request, response) => {
     response.status(200).json({'status': 'success'});
